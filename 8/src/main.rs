6a16cee17a1defbdc9bd7b83c3e63062a1db55db
Aaron McLeod <aaron.g.mcleod@gmail.com>
2020-12-10 20:20:21 -0500

    day 8 p2

--- 8/src/main.rs
+++ 8/src/main.rs
@@ -2,22 +2,11 @@ use std::collections::HashSet;
 
 use read_input::read_text;
 
-fn main() {
-    let text = read_text("8/input.txt").unwrap();
-
-    let mut commands = Vec::new();
-
-    for line in text.lines() {
-        let mut iter = line.split(" ");
-        let cmd = iter.next().unwrap();
-        let amt: i32 = iter.next().unwrap().parse().unwrap();
-
-        commands.push((cmd, amt));
-    }
-
+fn run_program(commands: &Vec<(&str, i32)>) -> (i32, bool) {
     let mut index: i32 = 0;
     let mut acc = 0;
     let mut used_indexes = HashSet::new();
+    let mut exited_successfuly = false;
 
     loop {
         let (cmd, n) = commands.get(index as usize).unwrap();
@@ -40,7 +29,59 @@ fn main() {
             break
         }
         used_indexes.insert(index);
+
+        if index as usize >= commands.len() {
+            exited_successfuly = true;
+            break
+        }
     }
 
+    (acc, exited_successfuly)
+}
+
+fn main() {
+    let text = read_text("8/input.txt").unwrap();
+
+    let mut commands = Vec::new();
+
+    for line in text.lines() {
+        let mut iter = line.split(" ");
+        let cmd = iter.next().unwrap();
+        let amt: i32 = iter.next().unwrap().parse().unwrap();
+
+        commands.push((cmd, amt));
+    }
+
+    let (acc, _) = run_program(&commands);
     println!("{}", acc);
+
+    let mut index = 0;
+    loop {
+        let (cmd, amt) = commands.get(index).unwrap();
+
+        if *cmd == "nop" || *cmd == "jmp" {
+            let mut commands = commands.clone();
+            let mut should_run =  false;
+            if *cmd == "nop" && *amt != 0 {
+                commands[index].0 = "jmp";
+                should_run = true;
+            } else if *cmd == "jmp" {
+                commands[index].0 = "nop";
+                should_run = true;
+            }
+
+            if should_run {
+                let (acc, exited) = run_program(&commands);
+                if exited {
+                    println!("{}", acc);
+                    break
+                }
+            }
+        }
+
+        index += 1;
+        if index == commands.len() {
+            panic!("Couldnt find answer");
+        }
+    }
 }
