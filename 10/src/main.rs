cf59435b1737297f13701ea40bc4e88a5737c7f8
Aaron McLeod <aaron.g.mcleod@gmail.com>
2020-12-12 18:38:25 -0500

    day 10 array permutation version

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -1,30 +1,48 @@
 use read_input::read_text;
 
 fn p2(adapters: &Vec<usize>) {
-    let mut work: Vec<usize> = vec![0];
+    let mut permutations: Vec<Vec<usize>> = vec![vec![0]];
 
     let mut count = 0;
 
-    println!("{:?}", adapters);
+    while permutations.len() > 0 {
+        let mut permutations_to_ditch = Vec::new();
+        let mut permutations_to_add = Vec::new();
+        for (p_index, permutation) in permutations.iter_mut().enumerate() {
+            let index = *permutation.last().unwrap();
+            if index == adapters.len() - 1 {
+                count += 1;
+                permutations_to_ditch.push(p_index);
+                continue;
+            }
+            let n = adapters[index];
+            let next_indexes: Vec<usize> = (index + 1..=index + 3)
+                .filter(|i| {
+                    if *i >= adapters.len() {
+                        return false;
+                    }
 
-    while work.len() > 0 {
-        let index = work.pop().unwrap();
-        if index == adapters.len() - 1 {
-            count += 1;
-            continue;
-        }
-        let n = adapters[index];
-        let mut next_indexes = (index + 1..index + 3)
-            .filter(|i| {
-                if *i >= adapters.len() {
-                    return false;
+                    adapters[*i] - n <= 3
+                })
+                .collect();
+
+            if next_indexes.len() == 0 {
+                permutations_to_ditch.push(p_index);
+            } else {
+                for i in next_indexes.iter().skip(1) {
+                    let mut perm = permutation.clone();
+                    perm.push(*i);
+                    permutations_to_add.push(perm);
                 }
+                permutation.push(next_indexes[0]);
+            }
+        }
 
-                adapters[*i] - n <= 3
-            })
-            .collect();
+        for (offset_index, index_to_remove) in permutations_to_ditch.iter().enumerate() {
+            permutations.remove(*index_to_remove - offset_index);
+        }
 
-        work.append(&mut next_indexes);
+        permutations.append(&mut permutations_to_add);
     }
 
     println!("{}", count);
@@ -35,8 +53,9 @@ fn main() {
 
     let mut adapters: Vec<usize> = text.lines().map(|n| n.parse::<usize>().unwrap()).collect();
     adapters.sort();
+    adapters.insert(0, 0);
 
-    let mut ones = 1;
+    let mut ones = 0;
     let mut threes = 1;
     for a in adapters.windows(2) {
         let diff = a[1] - a[0];
