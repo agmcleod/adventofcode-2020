a855e6cb7a891c910db9c137a25920b2eb36a8c0
Aaron McLeod <aaron.g.mcleod@gmail.com>
2020-12-15 21:29:20 -0500

    day 11 p2

--- 11/src/main.rs
+++ 11/src/main.rs
@@ -48,21 +48,28 @@ fn change_state(layout: &HashMap<(i32, i32), char>, max_x: i32, max_y: i32) -> H
     new_state
 }
 
-fn get_neighbour_counts_p2(layout: &HashMap<(i32, i32), char>, x: i32, y: i32) -> (i32, i32) {
-    let mut empty_count = 0;
+fn get_neighbour_counts_p2(layout: &HashMap<(i32, i32), char>, x: i32, y: i32) -> i32 {
     let mut occupied_count = 0;
 
     for offset in &OFFSETS {
-        if let Some(ch) = layout.get(&(x + offset.0, y + offset.1)) {
-            if *ch == 'L' {
-                empty_count += 1;
-            } else if *ch == '#' {
-                occupied_count += 1;
+        let mut pos = (x, y);
+        loop {
+            pos.0 += offset.0;
+            pos.1 += offset.1;
+            if let Some(ch) = layout.get(&(pos.0, pos.1)) {
+                if *ch == '#' {
+                    occupied_count += 1;
+                    break
+                } else if *ch == 'L' {
+                    break
+                }
+            } else {
+                break
             }
         }
     }
 
-    (empty_count, occupied_count)
+    occupied_count
 }
 
 fn change_state_p2(layout: &HashMap<(i32, i32), char>, max_x: i32, max_y: i32) -> HashMap<(i32, i32), char> {
@@ -70,11 +77,14 @@ fn change_state_p2(layout: &HashMap<(i32, i32), char>, max_x: i32, max_y: i32) -
 
     for y in 0..max_y {
         for x in 0..max_x {
-            let (_empty_count, occupied_count) = get_neighbour_counts+p2(layout, x, y);
+            if *layout.get(&(x, y)).unwrap() == '.' {
+                continue
+            }
+            let occupied_count = get_neighbour_counts_p2(layout, x, y);
             let ch = layout.get(&(x, y)).unwrap();
             if *ch == 'L' && occupied_count == 0 {
                 new_state.insert((x, y), '#');
-            } else if *ch == '#' && occupied_count >= 4 {
+            } else if *ch == '#' && occupied_count >= 5 {
                 new_state.insert((x, y), 'L');
             }
         }
@@ -83,7 +93,7 @@ fn change_state_p2(layout: &HashMap<(i32, i32), char>, max_x: i32, max_y: i32) -
     new_state
 }
 
-fn get_initial_state(text: &String) -> HashMap<(i32, i32), char> {
+fn get_initial_state(text: &String) -> (HashMap<(i32, i32), char>, i32, i32) {
     let mut layout = HashMap::new();
 
     let mut y = 0;
@@ -96,16 +106,16 @@ fn get_initial_state(text: &String) -> HashMap<(i32, i32), char> {
         y += 1;
     }
 
-    layout
+    let max_x = text.lines().next().unwrap().chars().count() as i32;
+    let max_y = text.lines().count() as i32;
+
+    (layout, max_x, max_y)
 }
 
 fn main() {
     let text = read_text("11/input.txt").unwrap();
 
-    let mut layout = get_initial_state(&text);
-
-    let max_x = text.lines().next().unwrap().chars().count() as i32;
-    let max_y = text.lines().count() as i32;
+    let (mut layout, max_x, max_y) = get_initial_state(&text);
 
     loop {
         let next_state = change_state(&layout, max_x, max_y);
@@ -124,6 +134,8 @@ fn main() {
         sum
     }));
 
+    let (mut layout, max_x, max_y) = get_initial_state(&text);
+
     loop {
         let next_state = change_state_p2(&layout, max_x, max_y);
         if next_state == layout {
@@ -132,5 +144,32 @@ fn main() {
         } else {
             layout = next_state;
         }
+    }
+
+    println!("{}", layout.iter().fold(0, |sum, (_, ch)| {
+        if *ch == '#' {
+            return sum + 1;
+        }
+        sum
+    }));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_p2_neighbour_counts() {
+        let test_string = ".............\n.L.L.#.#.#.#.\n.............".to_string();
+
+        let (layout, _max_x, _max_y) = get_initial_state(&test_string);
+        let count = get_neighbour_counts_p2(&layout, 1, 1);
+        assert_eq!(count, 0);
+
+        let count = get_neighbour_counts_p2(&layout, 3, 1);
+        assert_eq!(count, 1);
+
+        let count = get_neighbour_counts_p2(&layout, 7, 1);
+        assert_eq!(count, 2);
     }
 }
