b52bc11ae06a328c1138cf5c0a1106a596427d70
Aaron McLeod <aaron.g.mcleod@gmail.com>
2020-12-19 00:41:41 -0500

    day 14 p2

--- 14/src/main.rs
+++ 14/src/main.rs
@@ -1,3 +1,4 @@
+use std::char;
 use std::collections::HashMap;
 
 use read_input::read_text;
@@ -8,6 +9,27 @@ fn number_to_binary(num: usize, mask: &Vec<char>) -> Vec<char> {
     leading.append(&mut binary_number);
 
     leading
+}
+
+fn get_next_permutations(indexes: &Vec<usize>, current_index: usize) -> Vec<Vec<(char, usize)>> {
+    if current_index + 1 == indexes.len() {
+        (0..=1)
+            .map(|i| vec![(char::from_digit(i, 10).unwrap(), indexes[current_index])])
+            .collect()
+    } else {
+        let next = get_next_permutations(indexes, current_index + 1);
+        let mut results = Vec::new();
+        for i in 0..=1 {
+            for opt in &next {
+                let mut permutations =
+                    vec![(char::from_digit(i, 10).unwrap(), indexes[current_index])];
+                permutations.append(&mut opt.clone());
+                results.push(permutations);
+            }
+        }
+
+        results
+    }
 }
 
 fn main() {
@@ -38,6 +60,35 @@ fn main() {
                 }
             }
             addresses.insert(numbers[0], binary_number);
+
+            // p2 work
+            let binary_number = number_to_binary(numbers[0], &mask);
+            let mut one_indexes = Vec::new();
+            let mut x_indexes = Vec::new();
+            for (i, ch) in mask.iter().enumerate() {
+                if *ch == '1' {
+                    one_indexes.push(i);
+                } else if *ch == 'X' {
+                    x_indexes.push(i);
+                }
+            }
+
+            let permutations = get_next_permutations(&x_indexes, 0);
+
+            for permutation in &permutations {
+                let mut bn = binary_number.clone();
+                for oi in &one_indexes {
+                    bn[*oi] = '1';
+                }
+
+                for (value, index) in permutation {
+                    bn[*index] = *value;
+                }
+
+                let binary_string = bn.iter().collect::<String>();
+                let addr = usize::from_str_radix(&binary_string, 2).unwrap();
+                addresses_p2.insert(addr, numbers[1]);
+            }
         }
     }
 
@@ -46,4 +97,11 @@ fn main() {
     });
 
     println!("{}", sum);
+
+    println!(
+        "{}",
+        addresses_p2
+            .iter()
+            .fold(0, |sum, (_addr, value)| { sum + value })
+    );
 }
