bfa98b7ec0fb1c384be8dc4abbad62f3bdc1ff81
Aaron McLeod <aaron.g.mcleod@gmail.com>
2022-01-06 11:34:32 -0500

    working on d20

--- 20/src/main.rs
+++ 20/src/main.rs
@@ -1,83 +1,82 @@
-use std::cmp::{Eq, Ord, Ordering, PartialEq, PartialOrd};
-use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::collections::HashSet;
 use std::fmt::Debug;
 
 use read_input::read_text;
 
+type TileData = Vec<Vec<String>>;
+
 #[derive(Clone)]
 struct Tile {
     id: String,
-    edges: Vec<String>,
-    data: HashMap<(i32, i32), char>,
+    data: TileData,
 }
 
 impl Debug for Tile {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "Tile: {}\n{}\nEdges:\n{}\n",
+            "Tile: {}\n{}\n\n",
             self.id,
-            (0..10)
-                .map(|y| {
-                    (0..10)
-                        .map(|x| self.data.get(&(x, y)).unwrap())
-                        .collect::<String>()
-                })
+            self.data
+                .iter()
+                .map(|row| { row.join("") })
                 .collect::<Vec<String>>()
                 .join("\n"),
-            self.edges.join("\n")
         )
     }
 }
 
-struct Connection {
-    tile_id: String,
-    side: String,
+fn flip_tile_data<'a>(grid: &mut TileData) {
+    for row in grid.iter_mut() {
+        row.reverse();
+    }
 }
 
-struct Work {
-    level: usize,
-    tile_id: String,
-    tiles: Vec<Tile>,
-    scanned_tiles: HashSet<String>,
-    tile_connections: HashMap<String, Vec<Connection>>,
-}
+fn rotate_tile_data_by_90(grid: &mut TileData) {
+    let mut rotated = Vec::new();
+    let n = grid.len();
 
-impl Work {
-    fn new(
-        level: usize,
-        tile_id: String,
-        tiles: Vec<Tile>,
-        scanned_tiles: HashSet<String>,
-        tile_connections: HashMap<String, Vec<Connection>>,
-    ) -> Self {
-        Work {
-            level,
-            tile_id,
-            tiles,
-            scanned_tiles,
-            tile_connections,
+    for i in 0..n {
+        rotated.push(Vec::new());
+        for j in 0..n {
+            rotated[i].push(grid[n - j - 1][i].clone());
         }
     }
+
+    *grid = rotated;
 }
 
-impl Ord for Work {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.level.cmp(&other.level)
-    }
-}
-
-impl PartialOrd for Work {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Eq for Work {}
-
-impl PartialEq for Work {
-    fn eq(&self, other: &Self) -> bool {
-        self.level == other.level
+fn do_tiles_align(tile_one: &Tile, tile_two: &Tile) -> bool {
+    if tile_one.data[0] == tile_two.data[9] {
+        true
+    } else if tile_one.data[9] == tile_two.data[0] {
+        true
+    } else if tile_one.data[0][0] == tile_two.data[0][9]
+        && tile_one.data[1][0] == tile_two.data[1][9]
+        && tile_one.data[2][0] == tile_two.data[2][9]
+        && tile_one.data[3][0] == tile_two.data[3][9]
+        && tile_one.data[4][0] == tile_two.data[4][9]
+        && tile_one.data[5][0] == tile_two.data[5][9]
+        && tile_one.data[6][0] == tile_two.data[6][9]
+        && tile_one.data[7][0] == tile_two.data[7][9]
+        && tile_one.data[8][0] == tile_two.data[8][9]
+        && tile_one.data[9][0] == tile_two.data[9][9]
+    {
+        true
+    } else if tile_one.data[0][9] == tile_two.data[0][0]
+        && tile_one.data[1][9] == tile_two.data[1][0]
+        && tile_one.data[2][9] == tile_two.data[2][0]
+        && tile_one.data[3][9] == tile_two.data[3][0]
+        && tile_one.data[4][9] == tile_two.data[4][0]
+        && tile_one.data[5][9] == tile_two.data[5][0]
+        && tile_one.data[6][9] == tile_two.data[6][0]
+        && tile_one.data[7][9] == tile_two.data[7][0]
+        && tile_one.data[8][9] == tile_two.data[8][0]
+        && tile_one.data[9][9] == tile_two.data[9][0]
+    {
+        true
+    } else {
+        false
     }
 }
 
@@ -86,70 +85,53 @@ fn main() {
 
     let mut tiles = Vec::new();
     let mut id = String::new();
-    let mut tile_data: HashMap<(i32, i32), char> = HashMap::new();
-    let mut y = 0;
+    let mut tile_data = Vec::new();
 
     for line in text.lines() {
         if line.contains("Tile") {
             id = line.replace("Tile ", "").replace(":", "");
         } else if line == "" {
-            let mut edges = Vec::new();
-            edges.push(
-                (0..10)
-                    .map(|x| tile_data.get(&(x, 0)).unwrap().clone())
-                    .collect::<String>(),
-            );
-            edges.push(
-                (0..10)
-                    .map(|y| tile_data.get(&(9, y)).unwrap().clone())
-                    .collect::<String>(),
-            );
-            edges.push(
-                (0..10)
-                    .map(|x| tile_data.get(&(x, 9)).unwrap().clone())
-                    .collect::<String>(),
-            );
-            edges.push(
-                (0..10)
-                    .map(|y| tile_data.get(&(0, y)).unwrap().clone())
-                    .collect::<String>(),
-            );
             tiles.push(Tile {
                 id: id.clone(),
-                edges,
                 data: tile_data.clone(),
             });
-            y = 0;
             tile_data.clear();
-            continue;
         } else {
-            for (x, ch) in line.chars().enumerate() {
-                tile_data.insert((x as i32, y), ch);
+            let mut row = Vec::new();
+            for ch in line.chars() {
+                row.push(ch.to_string());
             }
-            y += 1;
+
+            tile_data.push(row);
         }
     }
 
-    let mut work_heap = BinaryHeap::new();
-    work_heap.push(Work::new(
-        0,
-        tiles[0].id.clone(),
-        tiles.clone(),
-        HashSet::new(),
-        HashMap::new(),
-    ));
-    loop {
-        let next_work = work_heap.pop();
-
-        if next_work.is_none() {
-            break;
-        }
-
-        let work = next_work.unwrap();
-
-        for tile in &tiles {
-            if work.scanned_tiles.contains(&tile.id) {
+    for tile in &tiles {
+        let mut used_tiles = HashSet::new();
+        for other_tile in &tiles {
+            if other_tile.id == tile.id {
                 continue;
+            }
+
+            let mut other_tile = other_tile.clone();
+
+            let mut count = 0;
+            let mut found = true;
+            while !do_tiles_align(tile, &other_tile) {
+                rotate_tile_data_by_90(&mut other_tile.data);
+                if count == 4 {
+                    flip_tile_data(&mut other_tile.data);
+                }
+
+                if count >= 7 {
+                    found = false;
+                    break;
+                }
+                count += 1;
+            }
+
+            if found {
+                used_tiles.insert(other_tile.id);
             }
         }
     }
