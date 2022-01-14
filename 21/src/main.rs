545d8fdc4d810d09abd11c2d0d7f5d57fbe48cc5
Aaron McLeod <aaron.g.mcleod@gmail.com>
2022-01-14 10:32:26 -0500

    some effort on d21

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use std::io::Result;
 
 use read_input::read_text;
@@ -5,18 +6,63 @@ use read_input::read_text;
 fn main() -> Result<()> {
     let text = read_text("21/input.txt")?;
 
-    for line in text.lines() {
+    let mut singular_allergens = HashMap::new();
+
+    let mut list = Vec::new();
+    for (i, line) in text.lines().enumerate() {
         let mut iter = line.split(" (");
-        let ingredients = iter.next().unwrap().split(" ").collect::<Vec<&str>>();
+        let ingredients = iter
+            .next()
+            .unwrap()
+            .split(" ")
+            .map(|v| v.to_string())
+            .collect::<Vec<String>>();
         let allergens = iter
             .next()
             .unwrap()
             .replace(")", "")
             .replace("contains ", "");
-        let allergens = allergens.split(", ").collect::<Vec<&str>>();
+        let allergens = allergens
+            .split(", ")
+            .map(|v| v.to_string())
+            .collect::<Vec<String>>();
 
-        println!("{:?} {:?}", ingredients, allergens);
+        if allergens.len() == 1 {
+            singular_allergens.insert(allergens[0].clone(), ingredients.clone());
+        }
+
+        list.push((ingredients, allergens));
     }
+
+    let mut scanned_ingredients = HashSet::new();
+    let mut no_allergen_ingredients = Vec::new();
+    for line in &list {
+        if line.1.len() == 1 {
+            continue;
+        }
+        // check each ingredient on this line
+        for ing in &line.0 {
+            // only check it if we have yet to scan it
+            if !scanned_ingredients.contains(ing) {
+                // check if the ingredient is part of other lines including just each allergen individually
+                let count_in_singular = line.1.iter().fold(0, |sum, allergen| {
+                    if let Some(other_ingr) = singular_allergens.get(allergen) {
+                        sum + other_ingr.iter().filter(|v| *v == ing).count()
+                    } else {
+                        sum
+                    }
+                });
+
+                if count_in_singular == 0 {
+                    no_allergen_ingredients.push(ing.clone());
+                }
+
+                scanned_ingredients.insert(ing.clone());
+            }
+        }
+    }
+
+    println!("{:?}", no_allergen_ingredients);
 
     Ok(())
 }
