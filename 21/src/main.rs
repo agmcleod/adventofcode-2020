e46f3470bf0c138b62316d17d71880e8b18102cf
Aaron McLeod <aaron.g.mcleod@gmail.com>
2022-01-16 23:02:38 -0500

    trying an allergen scan

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -7,6 +7,8 @@ fn main() -> Result<()> {
     let text = read_text("21/input.txt")?;
 
     let mut singular_allergens = HashMap::new();
+    let mut ingredients_map = HashMap::new();
+    let mut allergens_map = HashMap::new();
 
     let mut list = Vec::new();
     for line in text.lines() {
@@ -27,54 +29,97 @@ fn main() -> Result<()> {
             .map(|v| v.to_string())
             .collect::<Vec<String>>();
 
+        for allergen in &allergens {
+            let map = if allergens_map.contains_key(allergen) {
+                allergens_map.get_mut(allergen).unwrap()
+            } else {
+                allergens_map.insert(allergen.to_owned(), HashSet::new());
+                allergens_map.get_mut(allergen).unwrap()
+            };
+            for ing in &ingredients {
+                map.insert(ing.to_owned());
+            }
+        }
+
         if allergens.len() == 1 {
-            singular_allergens.insert(allergens[0].clone(), ingredients.clone());
+            let allergen_key = allergens.get(0).unwrap();
+            if singular_allergens.contains_key(allergen_key) {
+                let set: &mut HashSet<String> = singular_allergens.get_mut(allergen_key).unwrap();
+                for ing in &ingredients {
+                    set.insert(ing.clone());
+                }
+            } else {
+                let mut set = HashSet::new();
+                for ing in &ingredients {
+                    set.insert(ing.clone());
+                }
+                singular_allergens.insert(allergen_key.to_owned(), set);
+            }
+        }
+
+        for ing in &ingredients {
+            for allergen in &allergens {
+                let map = if ingredients_map.contains_key(ing) {
+                    ingredients_map.get_mut(ing).unwrap()
+                } else {
+                    ingredients_map.insert(ing.to_owned(), HashSet::new());
+                    ingredients_map.get_mut(ing).unwrap()
+                };
+
+                map.insert(allergen.to_owned());
+            }
         }
 
         list.push((ingredients, allergens));
     }
 
-    let mut no_allergen_ingredients = Vec::new();
-    let mut ingredients_mapped = HashSet::new();
+    // go through each allergen to figure out its potential owners
+    for (allergen, allergen_ingredients) in &allergens_map {
+        // get the list of ingredients where one of these MUST include the allergen
+        let required_ingredients = singular_allergens.get(allergen).unwrap();
+        for line in &list {
+            // if the line includes the current allergen
+            if line.1.contains(allergen) {
+                // if allergen list is 1, we can skip
+                if line.1.len() == 1 {
+                    continue;
+                }
 
-    for (i, line) in list.iter().enumerate() {
-        // might be able to do log(n) instead of o(n)^2 here
-        for (j, line2) in list.iter().enumerate() {
-            if i == j {
-                continue;
+                // check if the ingredients align with the required ones
+                for ingredient in &line.0 {
+                    if !required_ingredients.contains(ingredient) {
+                        // cannot contain the current allergen
+                        ingredients_map
+                            .get_mut(ingredient)
+                            .unwrap()
+                            .remove(allergen);
+                    }
+                }
             }
+        }
+
+        // see how many ingredients are using this allergen now
+        let mut allergen_usage_count = 0;
+        let mut ingredient_holding_allergen = String::new();
+        for ing in allergen_ingredients {
+            if ingredients_map.get(ing).unwrap().contains(allergen) {
+                allergen_usage_count += 1;
+                // store the ingredient key so we can clear out other allergens after
+                // will ovewrite in loop, but if count is > 1, doesn't matter anyways
+                ingredient_holding_allergen = ing.clone();
+            }
+        }
+
+        // if count is 1, remove any other allergens from that ingredient
+        if allergen_usage_count == 1 {
+            let mut new_set = HashSet::new();
+            new_set.insert(allergen.to_owned());
+            // replace the set with one just holding the single allergen
+            ingredients_map.insert(ingredient_holding_allergen, new_set);
         }
     }
 
-    // let mut scanned_ingredients = HashSet::new();
-    // let mut no_allergen_ingredients = Vec::new();
-    // for line in &list {
-    //     if line.1.len() == 1 {
-    //         continue;
-    //     }
-    //     // check each ingredient on this line
-    //     for ing in &line.0 {
-    //         // only check it if we have yet to scan it
-    //         if !scanned_ingredients.contains(ing) {
-    //             // check if the ingredient is part of other lines including just each allergen individually
-    //             let count_in_singular = line.1.iter().fold(0, |sum, allergen| {
-    //                 if let Some(other_ingr) = singular_allergens.get(allergen) {
-    //                     sum + other_ingr.iter().filter(|v| *v == ing).count()
-    //                 } else {
-    //                     sum
-    //                 }
-    //             });
-
-    //             if count_in_singular == 0 {
-    //                 no_allergen_ingredients.push(ing.clone());
-    //             }
-
-    //             scanned_ingredients.insert(ing.clone());
-    //         }
-    //     }
-    // }
-
-    // println!("{:?}", no_allergen_ingredients);
+    println!("{:?}", ingredients_map);
 
     Ok(())
 }
