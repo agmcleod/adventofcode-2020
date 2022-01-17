4643d04c917dc3960aef32fa001327c99f4f3ade
Aaron McLeod <aaron.g.mcleod@gmail.com>
2022-01-16 19:57:09 -0500

    day21 starting

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -9,7 +9,7 @@ fn main() -> Result<()> {
     let mut singular_allergens = HashMap::new();
 
     let mut list = Vec::new();
-    for (i, line) in text.lines().enumerate() {
+    for line in text.lines() {
         let mut iter = line.split(" (");
         let ingredients = iter
             .next()
@@ -34,35 +34,46 @@ fn main() -> Result<()> {
         list.push((ingredients, allergens));
     }
 
-    let mut scanned_ingredients = HashSet::new();
     let mut no_allergen_ingredients = Vec::new();
-    for line in &list {
-        if line.1.len() == 1 {
-            continue;
-        }
-        // check each ingredient on this line
-        for ing in &line.0 {
-            // only check it if we have yet to scan it
-            if !scanned_ingredients.contains(ing) {
-                // check if the ingredient is part of other lines including just each allergen individually
-                let count_in_singular = line.1.iter().fold(0, |sum, allergen| {
-                    if let Some(other_ingr) = singular_allergens.get(allergen) {
-                        sum + other_ingr.iter().filter(|v| *v == ing).count()
-                    } else {
-                        sum
-                    }
-                });
 
-                if count_in_singular == 0 {
-                    no_allergen_ingredients.push(ing.clone());
-                }
-
-                scanned_ingredients.insert(ing.clone());
+    for (i, line) in list.iter().enumerate() {
+        // might be able to do log(n) instead of o(n)^2 here
+        for (j, line2) in list.iter().enumerate() {
+            if i == j {
+                continue;
             }
         }
     }
 
-    println!("{:?}", no_allergen_ingredients);
+    // let mut scanned_ingredients = HashSet::new();
+    // let mut no_allergen_ingredients = Vec::new();
+    // for line in &list {
+    //     if line.1.len() == 1 {
+    //         continue;
+    //     }
+    //     // check each ingredient on this line
+    //     for ing in &line.0 {
+    //         // only check it if we have yet to scan it
+    //         if !scanned_ingredients.contains(ing) {
+    //             // check if the ingredient is part of other lines including just each allergen individually
+    //             let count_in_singular = line.1.iter().fold(0, |sum, allergen| {
+    //                 if let Some(other_ingr) = singular_allergens.get(allergen) {
+    //                     sum + other_ingr.iter().filter(|v| *v == ing).count()
+    //                 } else {
+    //                     sum
+    //                 }
+    //             });
+
+    //             if count_in_singular == 0 {
+    //                 no_allergen_ingredients.push(ing.clone());
+    //             }
+
+    //             scanned_ingredients.insert(ing.clone());
+    //         }
+    //     }
+    // }
+
+    // println!("{:?}", no_allergen_ingredients);
 
     Ok(())
 }
